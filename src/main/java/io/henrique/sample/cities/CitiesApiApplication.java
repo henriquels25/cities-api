a8a4dbe3f256e445f1c2e836f5a56aa3f102bff8
Henrique Schmidt <hschmidt@avenuecode.com>
2020-10-23 22:07:11 -0300

    First commit

---
+++ src/main/java/io/henrique/sample/cities/CitiesApiApplication.java
@@ -0,0 +1,13 @@
+package io.henrique.sample.cities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/henrique/sample/cities/CitiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.henrique.sample.cities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
